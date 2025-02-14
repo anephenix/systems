2dcb93e3d1e09906a47d75e930362147a5b1135c
Paul Jensen <paulbjensen@gmail.com>
2025-02-14 22:06:59 +0000

    Added tests for loops

--- test/lib/loop.test.ts
+++ test/lib/loop.test.ts
@@ -1,7 +1,30 @@
+import { Loop } from "../../dist/lib";
+import assert from "assert";
+
 describe('loop', () => {
-	it('should have a unique id');
-	it('can have a name');
-	it('should have a type of either balancing or reinforcing');
-	it('can have a list of entities');
-	it('can have a list of relations');
+
+	const loop = new Loop({ name: 'compounding interest', type: 'reinforcing', entities: [], relations: [] });
+	const loop2 = new Loop({ name: 'interest rates and inflation', type: 'balancing', entities: [], relations: [] });
+
+	it('should have a unique id', () => {
+		assert(loop.id !== undefined);
+		assert(loop.id.length === 36);
+	});
+	it('can have a name', () => {
+		assert.strictEqual(loop.name, 'compounding interest');
+	});
+
+	it('should have a type of either balancing or reinforcing', () => {
+		assert.strictEqual(loop.type, 'reinforcing');
+		assert.strictEqual(loop2.type, 'balancing');
+	});
+
+	it('can have a list of entities', () => {
+		assert.deepStrictEqual(loop.entities, []);
+	});
+
+	it('can have a list of relations', () => {
+		assert.deepStrictEqual(loop.relations, []);
+	});
+
 });
